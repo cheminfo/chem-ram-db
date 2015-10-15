ead781faa8624b362437689d2d2235bf91c6413e
Michaël Zasso <mic.besace@gmail.com>
2015-10-15 15:10:35 +0200

    add properties

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # chem-ram-db
+
+## CRD format, version 1
+
+### Header
+
+* Uint16: version of the format
+* Uint32: number of entries
+
+### Entry
+
+* Uint32: molecule ID in the database
+* Uint16: length of oclID (n)
+* Uint8[n]: oclID
+* Uint32[16]: index
+* Float32: absolute weight
+* Float32: relative weight
+* Float32: logP
+* Float32: logS
+* Float32: PSA
+* Uint32: acceptor count
+* Uint32: donor count
+* Uint32: rotatable bond count
+* Uint32: stereo center count
--- src/parse/sdf.js
+++ src/parse/sdf.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const CRD_VERSION = 1;
+
 const sdfStream = require('./SDFStream');
 const OutputBuffer = require('iobuffer').OutputBuffer;
 const Molecule = require('openchemlib').Molecule;
@@ -15,7 +17,8 @@ module.exports = function parseSDF(stream, options) {
         }
         const tsv = [];
         const crd = new OutputBuffer();
-        crd.skip(4); // we will put the size in the first 4 bytes but it is currently unknown
+        crd.writeUint16(CRD_VERSION);
+        crd.skip(4); // we will put the size here but it is currently unknown
         let total = 0;
         stream = stream.pipe(sdfStream());
         stream.on('data', function (mol) {
@@ -27,10 +30,28 @@ module.exports = function parseSDF(stream, options) {
             crd.writeUint32(id);
             crd.writeUint16(oclid.length);
             crd.writeChars(oclid);
+            // Index
+            const index = molecule.getIndex();
+            for (let i = 0; i < 16; i++) {
+                crd.writeUint32(index[i]);
+            }
+            // MF
+            const mf = molecule.getMolecularFormula();
+            crd.writeFloat32(mf.getAbsoluteWeight());
+            crd.writeFloat32(mf.getRelativeWeight());
+            // Props
+            const props = molecule.getProperties();
+            crd.writeFloat32(props.getLogP());
+            crd.writeFloat32(props.getLogS());
+            crd.writeFloat32(props.getPolarSurfaceArea());
+            crd.writeUint32(props.getAcceptorCount());
+            crd.writeUint32(props.getDonorCount());
+            crd.writeUint32(props.getRotatableBondCount());
+            crd.writeUint32(props.getStereoCenterCount());
         });
         stream.on('end', function () {
             crd.mark(); // todo hack because of https://github.com/image-js/iobuffer/issues/4
-            crd.rewind();
+            crd.seek(2); // go back to beginning and write number of entries
             crd.writeUint32(total);
             crd.reset();
             resolve({
